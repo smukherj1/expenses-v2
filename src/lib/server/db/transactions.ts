0f8368ac85992eb754cf4f3131111799e37b1d04
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-09-07 14:34:57 -0400

    Fix pagination sql logic for prev page

--- src/lib/server/db/transactions.ts
+++ src/lib/server/db/transactions.ts
@@ -1,6 +1,6 @@
 import { db } from "./client";
 import { transactionsTable } from "./schema";
-import { and, asc, eq, gte, gt, lte, or, SQL } from "drizzle-orm";
+import { and, asc, eq, gte, gt, lte, lt, or, SQL, desc } from "drizzle-orm";
 import {
   NewTxn,
   GetTxnsOpts,
@@ -42,49 +42,38 @@ const DefaultGetTxnOpts: GetTxnsOpts = {
 export async function GetTxns(
   popts: Partial<GetTxnsOpts>
 ): Promise<TxnsResult> {
-  const opts: GetTxnsOpts = {
-    from: popts.from || DefaultGetTxnOpts.from,
-    to: popts.to || DefaultGetTxnOpts.to,
-    desc: popts.desc || DefaultGetTxnOpts.desc,
-    descOp: popts.descOp || DefaultGetTxnOpts.descOp,
-    amount: popts.amount || DefaultGetTxnOpts.amount,
-    amountOp: popts.amountOp || DefaultGetTxnOpts.amountOp,
-    inst: popts.inst || DefaultGetTxnOpts.inst,
-    instOp: popts.instOp || DefaultGetTxnOpts.instOp,
-    pageSize: popts.pageSize || DefaultGetTxnOpts.pageSize,
-    prev: popts.prev || DefaultGetTxnOpts.prev,
-    next: popts.next || DefaultGetTxnOpts.next,
-  };
-  console.log(
-    `GetTxns(popts=${JSON.stringify(popts)}, opts=${JSON.stringify(opts)})`
-  );
-  if (opts.pageSize < 0) {
-    throw new Error(
-      `invalid pageSize given to GetTxns, got ${opts.pageSize}, want <= 0`
-    );
-  }
-  if (opts.prev && opts.next) {
-    throw new Error(
-      `prev=${opts.prev} and next=${JSON.stringify(opts.next)} can't be specified at the same time, only one of them or neither must be specified when getting transactions`
-    );
-  }
-  const baseConditions = [
+  const opts = validateOptsOrThrow(popts);
+
+  // Conditions that select transactions across all pages.
+  const allPagesConditions = [
     gte(transactionsTable.date, opts.from.getTime()),
     lte(transactionsTable.date, opts.to.getTime()),
   ];
-  let countQ = db.$count(transactionsTable, and(...baseConditions));
+  let countQ = db.$count(transactionsTable, and(...allPagesConditions));
   let baseQ = db
     .select()
     .from(transactionsTable)
     .where(
       and(
-        ...baseConditions,
+        ...allPagesConditions,
         pageConditions({ prev: opts.prev, next: opts.next })
       )
-    )
-    .orderBy(asc(transactionsTable.date), asc(transactionsTable.id));
+    );
+
+  const qWithOrder = opts.prev
+    ? baseQ.orderBy(desc(transactionsTable.date), desc(transactionsTable.id))
+    : baseQ.orderBy(asc(transactionsTable.date), asc(transactionsTable.id));
+
   const limit = opts.pageSize > 0 ? opts.pageSize : undefined;
-  const q = limit ? baseQ.limit(limit) : baseQ;
+  const qWithLimit = limit ? qWithOrder.limit(limit) : qWithOrder;
+
+  const sq = db.$with("sq").as(qWithLimit);
+
+  // In the final query to fetch the transactions, re-order them in ascending order
+  // by date and id for a consistent display order in the web UI. This is needed
+  // when displaying a "previous" page which would have sorted transactions in
+  // descending order to find the page.
+  const q = db.with(sq).select().from(sq).orderBy(asc(sq.date), asc(sq.id));
   console.log(
     `Running SQL: ${q.toSQL().sql} with params ${JSON.stringify(q.toSQL().params)}`
   );
@@ -106,6 +95,33 @@ export async function GetTxns(
     totalCount,
     ...prevAndNextFromTxns(txns),
   };
+}
+
+function validateOptsOrThrow(popts: Partial<GetTxnsOpts>): GetTxnsOpts {
+  const opts: GetTxnsOpts = {
+    from: popts.from || DefaultGetTxnOpts.from,
+    to: popts.to || DefaultGetTxnOpts.to,
+    desc: popts.desc || DefaultGetTxnOpts.desc,
+    descOp: popts.descOp || DefaultGetTxnOpts.descOp,
+    amount: popts.amount || DefaultGetTxnOpts.amount,
+    amountOp: popts.amountOp || DefaultGetTxnOpts.amountOp,
+    inst: popts.inst || DefaultGetTxnOpts.inst,
+    instOp: popts.instOp || DefaultGetTxnOpts.instOp,
+    pageSize: popts.pageSize || DefaultGetTxnOpts.pageSize,
+    prev: popts.prev || DefaultGetTxnOpts.prev,
+    next: popts.next || DefaultGetTxnOpts.next,
+  };
+  if (opts.pageSize < 0) {
+    throw new Error(
+      `invalid pageSize given to GetTxns, got ${opts.pageSize}, want <= 0`
+    );
+  }
+  if (opts.prev && opts.next) {
+    throw new Error(
+      `prev=${opts.prev} and next=${JSON.stringify(opts.next)} can't be specified at the same time, only one of them or neither must be specified when getting transactions`
+    );
+  }
+  return opts;
 }
 
 function prevAndNextFromTxns(txns: Txn[]): {
@@ -133,16 +149,24 @@ function pageConditions({
   prev?: TxnCursor;
   next?: TxnCursor;
 }): SQL | undefined {
-  if (!next) {
-    return;
+  if (next) {
+    return or(
+      gt(transactionsTable.date, next.date.getTime()),
+      and(
+        eq(transactionsTable.date, next.date.getTime()),
+        gt(transactionsTable.id, next.id)
+      )
+    );
   }
-  return or(
-    gt(transactionsTable.date, next.date.getTime()),
-    and(
-      eq(transactionsTable.date, next.date.getTime()),
-      gt(transactionsTable.id, next.id)
-    )
-  );
+  if (prev) {
+    return or(
+      lt(transactionsTable.date, prev.date.getTime()),
+      and(
+        eq(transactionsTable.date, prev.date.getTime()),
+        lt(transactionsTable.id, prev.id)
+      )
+    );
+  }
 }
 
 export async function DeleteTxns() {
