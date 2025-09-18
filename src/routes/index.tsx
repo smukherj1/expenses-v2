3b923598f4e182104cd33462bc8fdea28cc70b84
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-09-17 20:08:27 -0400

    Install shadcn charts. Add dummy chart to home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,38 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { type ChartConfig, ChartContainer } from "@/components/ui/chart";
+import { Bar, BarChart } from "recharts";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig;
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  return <>Home page</>;
+  return (
+    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+      <BarChart accessibilityLayer data={chartData}>
+        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+      </BarChart>
+    </ChartContainer>
+  );
 }
